b195e2e080c7167a42aadefaa8fac0e4ee728f45
Fernando Josue <fernandojossue05@mail.com>
2024-06-27 15:05:16 -0600

    27/06/2024

---
+++ funciones_2.py
@@ -0,0 +1,27 @@
+def sumar(numero1:int, numero2:int,):
+
+    return numero1 + numero2
+
+resultado = sumar(4,4)
+print(resultado)
+
+def sumar(numero3:int, numero4:int,):
+
+    return numero3 - numero4
+
+resultado = sumar(4,4)
+print(resultado)
+
+def sumar(numero5:int, numero6:int,):
+
+    return numero5 * numero6
+
+resultado = sumar(4,4)
+print(resultado)
+
+def sumar(numero7:int, numero8:int,):
+
+    return numero7 / numero8
+
+resultado = sumar(4,4)
+print(resultado)
---
+++ funciones_3.py
@@ -0,0 +1,7 @@
+def crear_usuario(nombre:str, apellido:str, f_nac:str):
+    nuevo_usuario= {
+        'nombre': nombre,
+        'apellido': apellido,
+        'f_nac': f_nac,
+        'estado': True
+    }
